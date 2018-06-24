5c21ab902669cb44fd76c6c26bbde86418fc60d9
Juan <jasobrino66@gmail.com>
2018-06-24 11:28:07 +0200

    Primera versión restserver

---
+++ README.md
@@ -0,0 +1,6 @@
+## proyecto curso para crear un REST server
+
+para instalación de los modulos de node:
+```
+npm install
+```
---
+++ server/config/config.js
@@ -0,0 +1,6 @@
+// ================================
+// Puerto
+// si no está definido en process
+// le ponemos el puerto local 3000
+// ================================
+process.env.PORT = process.env.PORT || 3000;
